1e8eab131773f53db03853a279cfba8cadf82863
Dennis Fricke <dennis@dennis-fricke.de>
2015-04-15 13:16:00 +0200

    added sidenav

--- scripts/application/main.js
+++ scripts/application/main.js
@@ -1,4 +1,5 @@
 /// <reference path="../typedef/angular.d.ts" />
+/// <reference path="../typedef/angular-material.d.ts" />
 angular.module("app", ['ngRoute', 'ngMaterial']).config([
     '$controllerProvider',
     function ($controllerProvider) {
@@ -20,4 +21,19 @@ angular.module("app", ['ngRoute', 'ngMaterial']).config([
 ]).config(['$mdIconProvider', function ($mdIconProvider) {
     $mdIconProvider.defaultIconSet("stylesheets/img/material-icons.svg", 24);
 }]);
+var WebEngineering;
+(function (WebEngineering) {
+    var MainController = (function () {
+        function MainController($scope, $mdSidenav) {
+            this.$scope = $scope;
+            this.$mdSidenav = $mdSidenav;
+            $scope.controller = this;
+        }
+        MainController.prototype.toggleSidenav = function () {
+            this.$mdSidenav('left').toggle();
+        };
+        return MainController;
+    })();
+    WebEngineering.MainController = MainController;
+})(WebEngineering || (WebEngineering = {}));
 //# sourceMappingURL=main.js.map
--- scripts/application/main.ts
+++ scripts/application/main.ts
@@ -1,4 +1,5 @@
 /// <reference path="../typedef/angular.d.ts" />
+/// <reference path="../typedef/angular-material.d.ts" />
 
 angular.module("app", ['ngRoute', 'ngMaterial']).config([
 	'$controllerProvider', function($controllerProvider) {
@@ -23,3 +24,16 @@ angular.module("app", ['ngRoute', 'ngMaterial']).config([
 	]).config(['$mdIconProvider', function ($mdIconProvider) {
 		$mdIconProvider.defaultIconSet("stylesheets/img/material-icons.svg", 24);
 	}]);
+
+module WebEngineering {
+	export class MainController{
+		constructor(private $scope: any, private $mdSidenav: angular.material.MDSidenavService) {
+			$scope.controller = this;
+		}
+
+		public toggleSidenav()
+		{
+			this.$mdSidenav('left').toggle();
+		}
+	}
+}
